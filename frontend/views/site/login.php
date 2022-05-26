516ded4064d821b85ea6af67140333f0d1538565
Sardor Dushamov <dsq2190@gmail.com>
2022-05-26 15:45:55 +0300

    first commit

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -22,8 +22,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'first_name')->textInput(['autofocus' => true]) ?>
-                <?= $form->field($model, 'last_name')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
